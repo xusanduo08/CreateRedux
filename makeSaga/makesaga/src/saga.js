6c1cfc682a9e797695ccf7b544a2f819fb6922a9
xusanduo08 <2279190887@qq.com>
2019-02-13 16:53:51 +0800

    saga

--- makeSaga/makesaga/src/saga.js
+++ makeSaga/makesaga/src/saga.js
@@ -1,7 +1,60 @@
-export default function sagaMiddleware({ getState, dispatch }) {
-  return next => action => {
-    const result = next(action);
+import { nextTick } from "q";
 
-    return result;
+const TASK_CANCEL = Symbol('TASK_CANCEL');
+const CANCEL = Symbol('CANCEL');
+
+function proc(iterator, parentContext, cont){
+  const task = {
+    cancel: () => next(TASK_CANCEL)
+  }
+
+  cont.cancel = task.cancel;
+  next();
+
+  return task
+
+  function next(arg, isErr){
+    try{
+      let result;
+      if(isErr){
+        result = iterator.throw(arg);
+      } else if(arg === TASK_CANCEL){
+        next.cancel();
+        result = iterator.return(TASK_CANCEL)
+      } else {
+        result = iterator.next(arg)
+      }
+
+      if(!result.done){
+        digestEffect(result.value, next);
+      } else {
+        cont(result.value);
+      }
+    } catch(error){
+      cont(error, true)
+    }
+  }
+
+  function runEffect(effect, currCb){
+    const effectType = effect[0];
+    if(effectType === 'promise'){
+      resolvePromise(effect, ctx, currCb);
+    } else if(effectType === 'iterator'){
+      resolveIterator(iterator, ctx, currCb);
+    } else {
+      throw new Error('Unknown effect type');
+    }
+  }
+
+  function resolvePromise([effectType, promise], ctx, cb){
+    const cancelPromise = promise[CANCEL];
+    if(is.func(cancelPromise)){
+      cb.cancel = cancelPromise;
+    }
+    promise.then(cb, error=> cb(error, true));
+  }
+
+  function resolveIterator([effectType, iterator], ctx, cb){
+    proc(iterator, ctx, cb);
   }
 }
