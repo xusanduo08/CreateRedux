1dd8daa839c95c8583531e09d7119f3a75470d1b
xusanduo08 <2279190887@qq.com>
2019-02-14 15:35:43 +0800

    添加备注

--- makeSaga/makesaga/src/little-saga.js
+++ makeSaga/makesaga/src/little-saga.js
@@ -4,7 +4,7 @@ const isIterator = fn => fn && isFunc(fn.next);
 
 // entrance
 function* root(){
-    yield takeEvery('INCREMENT_ASYNC1', incrementAsync);
+    yield takeEvery('INCREMENT_ASYNC1', incrementAsync); // takeEvery最终返回的是一个对象
 }
 
 function incrementAsync(){
@@ -17,8 +17,8 @@ proc(root());
 function channel(){
     const subscribers = [];
     function take(sub, matcher){
-        sub[MATCH] = matcher;
-        subscribers.push(sub);
+        sub[MATCH] = matcher; // 这个matcher是一个匹配函数，如果匹配，函数返回true
+        subscribers.push(sub); // 存储
     }
     function put(item){
         const arr = subscribers.slice();
@@ -42,14 +42,14 @@ const chan = new channel();
 function proc(iterator, args){
     next();
     function next(){
-        let result = iterator.next();
+        let result = iterator.next();// result.value就是takeEvery返回的obj，有type属性
         if(!result.done){
-            runEffect(result.value, next);
+            runEffect(result.value, next); // result.value = {type:'fork', context: null, args:[type, fn], fn: takeEveryHelper}
         } else {
             console.log('done')
         }
     }
-    function runEffect(obj, cb){
+    function runEffect(obj, cb){ // 这地方的obj是一个task, cb是next，next方法就是上面定义的
         if(obj.type == 'fork'){
             runForkEffect(obj, cb);
         } else if(obj.type =='take'){
@@ -57,18 +57,18 @@ function proc(iterator, args){
         }
     }
 
-    function runForkEffect({context, fn, args}, cb){
+    function runForkEffect({context/**null*/, fn/*takeEveryHelper*/, args/*[type, fn]*/}, cb/**next */){
         function createTaskIterator({context, fn, args}){
             if(isIterator(fn)){
                 return fn;
             }
-            let result = fn.apply(context, args);
+            let result = fn.apply(context, args); // 这个result={next}
             if(isIterator(result)){
                 return result;
             }
         }
         
-        let result = createTaskIterator({context, fn, args});
+        let result = createTaskIterator({context, fn, args});//result = {next}
         proc(result);
         cb();
     }
@@ -79,18 +79,34 @@ function proc(iterator, args){
 }
 
 function takeEvery(type, fn){
-    function takeEveryHepler(pattern, worker){
+    function takeEveryHepler(pattern, worker){ //入参是type, fn
         function fsmIterator(fsm, q0){
             const done = {done: true, value: undefined};
             const qEnd = {};
             let qNext=q0;
             function next(arg, error){
-                let [q, output] = fsm(qNext)()
+                let [q, output] = fsm[qNext]()
                 qNext = q;
                 return qNext === qEnd ? done : output;
             }
             return {next}
         }
+
+        return fsmIterator({
+            q1(){
+                return ['q2', {done: false, value: {type:'take', pattern}}]; //这地方的pattern就是action
+            },
+            q2(){
+                return ['q1', {done: false, value: {type:'fork', fn: worker}}]
+            }
+        }, 'q1')
+    }
+
+    return {
+        type:'fork',
+        context: null,
+        args: [type, fn],
+        fn: takeEveryHepler
     }
 
     
