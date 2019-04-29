e7c8ce01bca833f46e0352fc27c8d7be47ef1f8d
xusanduo08 <2279190887@qq.com>
2019-04-29 21:21:00 +0800

    for循环代替forEach

--- context/src/shallowEqual.js
+++ context/src/shallowEqual.js
@@ -2,7 +2,7 @@ function is(x, y){
   if(x === y){
     return x !== 0 || y !== 0 || 1/x === 1/y;
   } else {
-    return x != x || y != y;
+    return x !== x || y !== y;
   }
 }
 
@@ -11,17 +11,25 @@ function shallowEqual(objA, objB){
     return true;
   }
 
-  if(typeof objA != 'object' || typeof objB != 'object' 
+  if(typeof objA !== 'object' || typeof objB !== 'object' 
     || objA === null || objB === null){
       return false;
     }
 
   let keysA = Object.keys(objA);
   keysA.forEach(key => {
+    
     if(!is(objA[key], objB[key])){
       return false
     }
   })
+  for(let i = 0; i < keysA.length; i++){
+    if(!objB.hasOwnProperty(keysA[i])){
+      return false;
+    } else if(!is(objA[keysA[i]], objB[keysA[i]])){
+      return false
+    }
+  }
   return true;
 }
 
