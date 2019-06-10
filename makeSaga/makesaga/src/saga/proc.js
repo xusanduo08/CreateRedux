c203a5362cb6caa5df2fdde0074e0f1157f03261
xusanduo08 <2279190887@qq.com>
2019-06-10 21:54:20 +0800

    使用while循环来消费迭代器

--- makeSaga/makesaga/src/saga/proc.js
+++ makeSaga/makesaga/src/saga/proc.js
@@ -1,14 +1,19 @@
 
 // 专门处理iterator
 function proc(iterator, channel) {
-  let {done, value} = iterator.next();
-  if(!done){
-    // TODO
-    // 如果generator没有结束的话，那么需要做两件事
-    // 1.处理当前value中包含的操作
-    // 2.继续向下执行generator
-    channel.take(value.actionType, value.fn);
+  while(true){
+    let {done, value} = iterator.next();
+    if(!done){
+      // TODO
+      // 如果generator没有结束的话，那么需要做两件事
+      // 1.处理当前value中包含的操作
+      // 2.继续向下执行generator
+      channel.take(value.actionType, value.fn);
+    } else {
+      break;
+    }
   }
+  
   
 }
 
