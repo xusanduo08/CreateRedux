8eab09a2e112542e06fcb54b1a98756bfc54ed43
xusanduo08 <2279190887@qq.com>
2019-06-11 19:37:46 +0800

    测试

--- makeSaga/makesaga/src/App.js
+++ makeSaga/makesaga/src/App.js
@@ -4,22 +4,24 @@ import {connect} from 'react-redux';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.doAsyncWork = this.doAsyncWork.bind(this);
-  }
+  
   doAsyncWork(){
-    // this.props.dispatch({type:'START', seconds: 2000});/
     this.props.dispatch({
       type:'pSaga'
     })
   }
-  componentDidMount(){
+  
+  doWork2(){
+    this.props.dispatch({
+      type:'getInfo2'
+    })
   }
+
   render() {
     return (
       <div className="App">
-        <button onClick={this.doAsyncWork} id='btn'>DoAsyncWork</button>
+        <button onClick={this.doAsyncWork.bind(this)} id='btn'>DoAsyncWork</button>
+        <button onClick={this.doWork2.bind(this)} id='btn'>doWork2</button>
         <p>{this.props.text}</p>
       </div>
     );
--- makeSaga/makesaga/src/sagas.js
+++ makeSaga/makesaga/src/sagas.js
@@ -2,16 +2,6 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { counter } from './api';
 import { take } from './saga/effects.js';
 
-function* start(action) {
-  const m = yield call(counter, action.seconds)
-  console.log(m);
-  yield put({ type: 'END', payload: { text: 'timer ended' } })
-}
-
-function* mySaga() {
-  yield takeLatest('START', start);
-}
-
 async function getInfo(){
     let result = await new Promise(resolve => setTimeout(() => {
       resolve(333);
@@ -26,14 +16,27 @@ async function getInfo2(arg){
   return result;
 }
 
+async function getInfo3(arg){
+  let result = await new Promise(resolve => setTimeout(() => {
+    resolve(arg);
+  }, 3000));
+  return result;
+}
+
 function* pSagaTest() {
   const info = yield getInfo();
   const result = yield getInfo2(info);
   console.log(result);
 }
 
+function* sagaTest(){
+  const info = yield getInfo3();
+  console.log(info);
+}
+
 function* pSaga() {
   yield take('pSaga', pSagaTest);
+  yield take('getInfo2', sagaTest)
 }
 
 export default pSaga;
