7369d12c231c257e965a94efc635b1fe913ad879
xusanduo08 <2279190887@qq.com>
2019-06-18 19:25:31 +0800

    目录调整：独立effectRunner

---
+++ makeSaga/makesaga/src/saga/effectRunnerMap.js
@@ -0,0 +1,27 @@
+import {actionChannel} from './channel';
+import {TAKE, ACTION_CHANNEL} from './effectType';
+
+
+function runTakeEffect(env, { channel = env.channel, pattern }, cb) {
+  channel.take(cb, pattern, TAKE);
+}
+
+// 发送了一个action后，如果channel中没有接收的taker，则将这次消息缓存
+/**
+ * actionChannel会检测当前taker中有没有接收当前消息的take，没有的话则缓存，等待指定的taker
+ */
+function runChannelEffect(env, { pattern }, cb) {
+  const channel = actionChannel();
+
+  const taker = (action) => {
+    env.channel.take(taker, pattern, ACTION_CHANNEL);
+    channel.put(action);
+  };
+  env.channel.take(taker, pattern, ACTION_CHANNEL); // stdChannel负责根据action触发对应的操作
+  cb(channel);
+}
+
+export default {
+  TAKE: runTakeEffect,
+  ACTION_CHANNEL: runChannelEffect
+}
--- makeSaga/makesaga/src/saga/proc.js
+++ makeSaga/makesaga/src/saga/proc.js
@@ -1,6 +1,5 @@
 import {TAKE, ACTION_CHANNEL} from './effectType';
-import {actionChannel} from './channel';
-
+import effectRunnerMap from './effectRunnerMap';
 
 // 专门处理iterator
 function proc(iterator, env) {
@@ -22,30 +21,8 @@ function proc(iterator, env) {
   }
   
   function runEffect(effect, cb){
-    if(effect.type === TAKE){
-      runTakeEffect(env, effect.payload, cb);
-    } else if(effect.type === ACTION_CHANNEL){
-      runChannelEffect(env, effect.payload, cb);
-    }
-  }
-
-  function runTakeEffect(env, {channel = env.channel, pattern}, cb){
-    channel.take(cb, pattern, TAKE);
-  }
-
-  // 发送了一个action后，如果channel中没有接收的taker，则将这次消息缓存
-  /**
-   * actionChannel会检测当前taker中有没有接收当前消息的take，没有的话则缓存，等待指定的taker
-   */
-  function runChannelEffect(env, {pattern}, cb){
-    const channel = actionChannel();
-    
-    const taker = (action) => {
-      env.channel.take(taker, pattern, ACTION_CHANNEL);
-      channel.put(action);
-    };
-    env.channel.take(taker, pattern, ACTION_CHANNEL); // stdChannel负责根据action触发对应的操作
-    cb(channel);
+    let effectRunner = effectRunnerMap[effect.type];
+    effectRunner(env, effect.payload, cb);
   }
 
 }
