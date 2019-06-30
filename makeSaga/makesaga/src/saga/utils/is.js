54b5a308c66eac62980b07564823aa5e16dd6f0b
xusanduo08 <2279190887@qq.com>
2019-06-30 20:21:08 +0800

    先判断有没有，在判断符不符合条件

--- makeSaga/makesaga/src/saga/utils/is.js
+++ makeSaga/makesaga/src/saga/utils/is.js
@@ -1,9 +1,9 @@
 
 export const func = fn => typeof fn == 'function';
-export const iterator = fn => func(fn.next);
+export const iterator = fn => fn && func(fn.next);
 export const promise = p => p && func(p.then);
-export const channel = ch => func(ch.take) && func(ch.put)
+export const channel = ch => ch && func(ch.take) && func(ch.put)
 export const array = Array.isArray;
 export const string = s => typeof s === 'string';
-export const pattern = pat => typeof pat === 'string' || func(pat) || (array(pat) && pat.every(pattern));
+export const pattern = pat =>  pat && typeof pat === 'string' || func(pat) || (array(pat) && pat.every(pattern));
 export const undef = arg => arg === null || arg === undefined;
