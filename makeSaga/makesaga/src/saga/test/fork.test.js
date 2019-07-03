6eac2afb291e545cc0846583c1909c02413b95b1
xusanduo08 <2279190887@qq.com>
2019-07-03 18:22:35 +0800

    fork测试用例

---
+++ makeSaga/makesaga/src/saga/test/fork.test.js
@@ -0,0 +1,17 @@
+import {call, fork} from '../effects';
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from '../sagaMiddleware';
+
+test('should not interpret returned effect. fork(()=>effectCreator())', () => { // 不应该包装返回的effect类型
+  const middleware = createSagaMiddleware();
+  createStore(()=> {}, {}, applyMiddleware(middleware));
+  const fn = () => null;
+
+  function* genFn(){
+    const task = yield fork(()=> call(fn));
+    return task;
+  }
+
+  return middleware.run(genFn).toPromise().then(actual => expect(actual).toEqual(call(fn)));
+    
+})
