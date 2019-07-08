ed4a7c97e807886c21ed45133ae11b4a88abd10d
xusanduo08 <2279190887@qq.com>
2019-07-08 16:42:07 +0800

    test:take-effect-cancellation

---
+++ makeSaga/makesaga/src/saga/test/cancellation.test.js
@@ -0,0 +1,37 @@
+import { take, cancelled } from '../effects';
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from '../sagaMiddleware';
+import deffered from '../utils/deffered';
+
+test('saga cancellation: take effect', () => {
+  let actual = [];
+  let startDef = deffered();
+  let cancelDef = deffered();
+  const sagaMiddleware = createSagaMiddleware();
+  const store = applyMiddleware(sagaMiddleware)(createStore)(()=>{});
+
+  function * main(){
+    actual.push(yield startDef.promise);
+    try{
+      actual.push(yield take('action'));
+    } finally{
+      if(yield cancelled()){
+        actual.push('cancelled');
+      }
+    }
+  }
+
+  const task = sagaMiddleware.run(main);
+  cancelDef.promise.then(v => {
+    actual.push(v);
+    task.cancel();
+  })
+  Promise.resolve(1)
+    .then(()=> startDef.resolve('start'))
+    .then(()=> cancelDef.resolve('cancel'))
+    .then(() => {
+      store.dispatch('action')
+    })
+  const expected = ['start', 'cancel', 'cancelled'];
+  task.toPromise().then(() => expect(actual).toEqual(expected));
+})
