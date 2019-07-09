3c6be8fd19045bd6aa86dcf82990273b8ff9e126
xusanduo08 <2279190887@qq.com>
2019-07-09 22:21:38 +0800

    代码回退

--- makeSaga/makesaga/src/saga/test/cancellation.test.js
+++ makeSaga/makesaga/src/saga/test/cancellation.test.js
@@ -3,88 +3,88 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from '../sagaMiddleware';
 import deferred from '../utils/deferred';
 
-// test('saga cancellation: take effect', () => {
-//   let actual = [];
-//   let startDef = deferred();
-//   let cancelDef = deferred();
-//   const sagaMiddleware = createSagaMiddleware();
-//   const store = applyMiddleware(sagaMiddleware)(createStore)(()=>{});
+test('saga cancellation: take effect', () => {
+  let actual = [];
+  let startDef = deferred();
+  let cancelDef = deferred();
+  const sagaMiddleware = createSagaMiddleware();
+  const store = applyMiddleware(sagaMiddleware)(createStore)(()=>{});
 
-//   function * main(){
-//     actual.push(yield startDef.promise);
-//     try{
-//       actual.push(yield take('action'));
-//     } finally{
-//       if(yield cancelled()){
-//         actual.push('cancelled');
-//       }
-//     }
-//   }
+  function * main(){
+    actual.push(yield startDef.promise);
+    try{
+      actual.push(yield take('action'));
+    } finally{
+      if(yield cancelled()){
+        actual.push('cancelled');
+      }
+    }
+  }
 
-//   const task = sagaMiddleware.run(main);
-//   cancelDef.promise.then(v => {
-//     actual.push(v);
-//     task.cancel();
-//   })
-//   Promise.resolve(1)
-//     .then(()=> startDef.resolve('start'))
-//     .then(()=> cancelDef.resolve('cancel'))
-//     .then(() => {
-//       store.dispatch('action')
-//     })
-//   const expected = ['start', 'cancel', 'cancelled'];
-//   task.toPromise().then(() => expect(actual).toEqual(expected));
-// })
+  const task = sagaMiddleware.run(main);
+  cancelDef.promise.then(v => {
+    actual.push(v);
+    task.cancel();
+  })
+  Promise.resolve(1)
+    .then(()=> startDef.resolve('start'))
+    .then(()=> cancelDef.resolve('cancel'))
+    .then(() => {
+      store.dispatch('action')
+    })
+  const expected = ['start', 'cancel', 'cancelled'];
+  task.toPromise().then(() => expect(actual).toEqual(expected));
+})
 
-// test('saga cancellation: call effect', () => {
-//   let actual = [];
-//   const sagaMiddleware = createSagaMiddleware();
-//   createStore(()=>{}, {}, applyMiddleware(sagaMiddleware));
-//   let startDef = deferred();
-//   let cancelDef = deferred();
-//   let subroutineDef = deferred();
+test('saga cancellation: call effect', () => {
+  let actual = [];
+  const sagaMiddleware = createSagaMiddleware();
+  createStore(()=>{}, {}, applyMiddleware(sagaMiddleware));
+  let startDef = deferred();
+  let cancelDef = deferred();
+  let subroutineDef = deferred();
 
-//   function * main(){
-//     actual.push(yield startDef.promise);
+  function * main(){
+    actual.push(yield startDef.promise);
 
-//     try{
-//       actual.push(yield call(subroutine));
-//     } finally{
-//       if(yield cancelled()){
-//         actual.push('cancelled');
-//       }
-//     }
-//   }
+    try{
+      actual.push(yield call(subroutine));
+    } finally{
+      if(yield cancelled()){
+        actual.push('cancelled');
+      }
+    }
+  }
 
-//   function* subroutine(){
-//     actual.push(yield 'subroutine start');
+  function* subroutine(){
+    actual.push(yield 'subroutine start');
 
-//     try{
-//       actual.push(yield subroutineDef.promise)
-//     } finally {
-//       if(yield cancelled()){
-//         actual.push(yield 'subroutine cancelled');
-//       }
-//     }
-//   }
+    try{
+      actual.push(yield subroutineDef.promise)
+    } finally {
+      if(yield cancelled()){
+        actual.push(yield 'subroutine cancelled');
+      }
+    }
+  }
 
-//   Promise.resolve(1)
-//     .then(startDef.resolve('start'))
-//     .then(cancelDef.resolve('cancel'))
-//     .then(subroutineDef.resolve('subroutine'))
+  Promise.resolve(1)
+    .then(startDef.resolve('start'))
+    .then(cancelDef.resolve('cancel'))
+    .then(subroutineDef.resolve('subroutine'))
 
-//   const task = sagaMiddleware.run(main);
-//   cancelDef.promise.then(v => {
-//     actual.push(v);
-//     task.cancel();
-//   })
+  const task = sagaMiddleware.run(main);
+  cancelDef.promise.then(v => {
+    actual.push(v);
+    task.cancel();
+  })
 
-//   const expected = ['start', 'subroutine start', 'cancel', 'subroutine cancelled', 'cancelled'];
-//   return task.toPromise().then(()=>{
-//     expect(actual).toEqual(expected);
-//   })
+  const expected = ['start', 'subroutine start', 'cancel', 'subroutine cancelled', 'cancelled'];
+  return task.toPromise().then(()=>{
+    expect(actual).toEqual(expected);
+  })
 
-// })
+})
 
 test('saga cancellation: forked children', () => {
   const actual = [];
