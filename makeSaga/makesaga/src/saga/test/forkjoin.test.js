8d4c52a65b656737d8af232acaa823f47735b73f
xusanduo08 <2279190887@qq.com>
2019-07-16 16:41:57 +0800

    测试

--- makeSaga/makesaga/src/saga/test/forkjoin.test.js
+++ makeSaga/makesaga/src/saga/test/forkjoin.test.js
@@ -1,4 +1,4 @@
-import { call, fork, take, join, cancelled } from '../effects';
+import { call, fork, take, join, cancelled, cancel } from '../effects';
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from '../sagaMiddleware';
 import deferred from '../utils/deferred';
@@ -243,6 +243,290 @@ test('saga auto cancel forks on error', () => {
   ]
   return task.toPromise().then(() => {
     // parent task must cancel all forked tasks when it aborts
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('saga auto cancel forks on main cancelled', () => {
+  const middleware = createSagaMiddleware()
+  createStore(() => ({}), {}, applyMiddleware(middleware))
+  const actual = []
+  const rootDef = deferred()
+  const mainDef = deferred()
+  const childAdef = deferred()
+  const childBdef = deferred()
+  const defs = [deferred(), deferred(), deferred(), deferred()]
+  Promise.resolve()
+    .then(() => childAdef.resolve('childA resolved'))
+    .then(() => defs[0].resolve('leaf 1 resolved'))
+    .then(() => childBdef.resolve('childB resolved'))
+    .then(() => defs[1].resolve('leaf 2 resolved'))
+    .then(() => rootDef.resolve('root resolved'))
+    .then(() => mainDef.resolve('main resolved'))
+    .then(() => defs[2].resolve('leaf 3 resolved'))
+    .then(() => defs[3].resolve('leaf 4 resolved'))
+
+  function* root() {
+    try {
+      const task = yield fork(main)
+      actual.push(yield rootDef.promise)
+      yield cancel(task)
+    } catch (e) {
+      actual.push('root caught ' + e)
+    }
+  }
+
+  function* main() {
+    try {
+      yield fork(childA)
+      yield fork(childB)
+      actual.push(yield mainDef.promise)
+    } finally {
+      if (yield cancelled()) actual.push('main cancelled')
+    }
+  }
+
+  function* childA() {
+    try {
+      yield fork(leaf, 0)
+      actual.push(yield childAdef.promise)
+      yield fork(leaf, 1)
+    } finally {
+      if (yield cancelled()) actual.push('childA cancelled')
+    }
+  }
+
+  function* childB() {
+    try {
+      yield fork(leaf, 2)
+      yield fork(leaf, 3)
+      actual.push(yield childBdef.promise)
+    } finally {
+      if (yield cancelled()) actual.push('childB cancelled')
+    }
+  }
+
+  function* leaf(idx) {
+    try {
+      actual.push(yield defs[idx].promise)
+    } finally {
+      if (yield cancelled()) actual.push(`leaf ${idx + 1} cancelled`)
+    }
+  }
+
+  const task = middleware.run(root)
+  const expected = [
+    'childA resolved',
+    'leaf 1 resolved',
+    'childB resolved',
+    'leaf 2 resolved',
+    'root resolved',
+    'main cancelled',
+    'leaf 3 cancelled',
+    'leaf 4 cancelled',
+  ]
+  return task.toPromise().then(() => {
+    // parent task must cancel all forked tasks when it's cancelled
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('saga auto cancel forks if a child aborts', () => {
+  const actual = []
+  const middleware = createSagaMiddleware()
+  createStore(() => ({}), {}, applyMiddleware(middleware))
+  const mainDef = deferred()
+  const childAdef = deferred()
+  const childBdef = deferred()
+  const defs = [deferred(), deferred(), deferred(), deferred()]
+  Promise.resolve()
+    .then(() => childAdef.resolve('childA resolved'))
+    .then(() => defs[0].resolve('leaf 1 resolved'))
+    .then(() => childBdef.resolve('childB resolved'))
+    .then(() => defs[1].resolve('leaf 2 resolved'))
+    .then(() => mainDef.resolve('main resolved'))
+    .then(() => defs[2].reject('leaf 3 error'))
+    .then(() => defs[3].resolve('leaf 4 resolved'))
+
+  function* root() {
+    try {
+      actual.push(yield call(main))
+    } catch (e) {
+      actual.push('root caught ' + e)
+    }
+  }
+
+  function* main() {
+    try {
+      yield fork(childA)
+      yield fork(childB)
+      actual.push(yield mainDef.promise)
+      return 'main returned'
+    } finally {
+      if (yield cancelled()) actual.push('main cancelled')
+    }
+  }
+
+  function* childA() {
+    try {
+      yield fork(leaf, 0)
+      actual.push(yield childAdef.promise)
+      yield fork(leaf, 1)
+    } finally {
+      if (yield cancelled()) actual.push('childA cancelled')
+    }
+  }
+
+  function* childB() {
+    try {
+      yield fork(leaf, 2)
+      yield fork(leaf, 3)
+      actual.push(yield childBdef.promise)
+    } finally {
+      if (yield cancelled()) actual.push('childB cancelled')
+    }
+  }
+
+  function* leaf(idx) {
+    try {
+      actual.push(yield defs[idx].promise)
+    } catch (e) {
+      actual.push(e)
+      throw e
+    } finally {
+      if (yield cancelled()) actual.push(`leaf ${idx + 1} cancelled`)
+    }
+  }
+
+  const task = middleware.run(root)
+  const expected = [
+    'childA resolved',
+    'leaf 1 resolved',
+    'childB resolved',
+    'leaf 2 resolved',
+    'main resolved',
+    'leaf 3 error',
+    'leaf 4 cancelled',
+    'root caught leaf 3 error',
+  ]
+  return task.toPromise().then(() => {
+    // parent task must cancel all forked tasks when it aborts
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('saga auto cancel parent + forks if a child aborts', () => {
+  const actual = []
+  const middleware = createSagaMiddleware()
+  createStore(() => ({}), {}, applyMiddleware(middleware))
+  const mainDef = deferred()
+  const childAdef = deferred()
+  const childBdef = deferred()
+  const defs = [deferred(), deferred(), deferred(), deferred()]
+  Promise.resolve()
+    .then(() => childAdef.resolve('childA resolved'))
+    .then(() => defs[0].resolve('leaf 1 resolved'))
+    .then(() => childBdef.resolve('childB resolved'))
+    .then(() => defs[1].resolve('leaf 2 resolved'))
+    .then(() => defs[2].reject('leaf 3 error'))
+    .then(() => mainDef.resolve('main resolved'))
+    .then(() => defs[3].resolve('leaf 4 resolved'))
+
+  function* root() {
+    try {
+      actual.push(yield call(main))
+    } catch (e) {
+      actual.push('root caught ' + e)
+    }
+  }
+
+  function* main() {
+    try {
+      yield fork(childA)
+      yield fork(childB)
+      actual.push(yield mainDef.promise)
+      return 'main returned'
+    } catch (e) {
+      actual.push(e)
+      throw e
+    } finally {
+      if (yield cancelled()) actual.push('main cancelled')
+    }
+  }
+
+  function* childA() {
+    try {
+      yield fork(leaf, 0)
+      actual.push(yield childAdef.promise)
+      yield fork(leaf, 1)
+    } finally {
+      if (yield cancelled()) actual.push('childA cancelled')
+    }
+  }
+
+  function* childB() {
+    try {
+      yield fork(leaf, 2)
+      yield fork(leaf, 3)
+      actual.push(yield childBdef.promise)
+    } finally {
+      if (yield cancelled()) actual.push('childB cancelled')
+    }
+  }
+
+  function* leaf(idx) {
+    try {
+      actual.push(yield defs[idx].promise)
+    } catch (e) {
+      actual.push(e)
+      throw e
+    } finally {
+      if (yield cancelled()) actual.push(`leaf ${idx + 1} cancelled`)
+    }
+  }
+
+  const task = middleware.run(root)
+  const expected = [
+    'childA resolved',
+    'leaf 1 resolved',
+    'childB resolved',
+    'leaf 2 resolved',
+    'leaf 3 error',
+    'leaf 4 cancelled',
+    'main cancelled',
+    'root caught leaf 3 error',
+  ]
+  return task.toPromise().then(() => {
+    // parent task must cancel all forked tasks when it aborts
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('joining multiple tasks', () => {
+  const defs = [deferred(), deferred(), deferred()]
+  let actual
+  const middleware = createSagaMiddleware()
+  createStore(() => ({}), {}, applyMiddleware(middleware))
+
+  function* worker(i) {
+    return yield defs[i].promise
+  }
+
+  function* genFn() {
+    const task1 = yield fork(worker, 0)
+    const task2 = yield fork(worker, 1)
+    const task3 = yield fork(worker, 2)
+    actual = yield join([task1, task2, task3])
+  }
+
+  const mainTask = middleware.run(genFn)
+  Promise.resolve()
+    .then(() => defs[0].resolve(1))
+    .then(() => defs[2].resolve(3))
+    .then(() => defs[1].resolve(2))
+  const expected = [1, 2, 3]
+  return mainTask.toPromise().then(() => {
+    // it must be possible to join on multiple tasks
     expect(actual).toEqual(expected)
   })
 })
